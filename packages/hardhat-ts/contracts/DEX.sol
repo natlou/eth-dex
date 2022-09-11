ad51bdd3f08d7e264cf40a9aa7099abe9ad4dedd
natlou <natloudev@gmail.com>
2022-09-11 15:03:02 -0400

    price function

--- packages/hardhat-ts/contracts/DEX.sol
+++ packages/hardhat-ts/contracts/DEX.sol
@@ -65,7 +65,12 @@ contract DEX {
         uint256 xInput,
         uint256 xReserves,
         uint256 yReserves
-    ) public view returns (uint256 yOutput) {}
+    ) public view returns (uint256 yOutput) {
+        uint256 xInput_fee_tax = xInput.mul(997);
+        uint256 numerator = xInput_fee_tax * yReserves;
+        uint256 denominator = (xReserves + xInput_fee_tax) * 1000;
+        return numerator / denominator;
+    }
 
     /**
      * @notice returns liquidity for a user. Note this is not needed typically due to the `liquidity()` mapping variable being public and having a getter as a result. This is left though as it is used within the front end code (App.jsx).
